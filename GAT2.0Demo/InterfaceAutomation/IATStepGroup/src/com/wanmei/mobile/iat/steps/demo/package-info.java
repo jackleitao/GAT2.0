226fc0f45e81326446440de6be1ec573d4738dc5
zhangtiande@pwrd.com <zhangtiande@pwrd.com>
2015-07-16 15:42:47 +0800

    delete IATStepGroup.jar

--- GAT2.0Demo/InterfaceAutomation/IATStepGroup/src/com/wanmei/mobile/iat/steps/demo/package-info.java
+++ GAT2.0Demo/InterfaceAutomation/IATStepGroup/src/com/wanmei/mobile/iat/steps/demo/package-info.java
@@ -3,14 +3,6 @@
  */
 /**   
  *    
- * Project：IATStepGroup   
- * Class：package-info   
- * Desc：   
- * Creator：zhangtiande   
- * Time：2013年11月11日 下午3:26:22   
- * Modified by：zhangtiande   
- * Modified time：2013年11月11日 下午3:26:22   
- * Modified comments：   
  * @version    
  *    
  */
